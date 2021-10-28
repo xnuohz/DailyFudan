ab03c5563bfd18a5826fbeef89e6adfc1240f9f5
pickerxxr <18307100052@fudan.edu.cn>
2021-10-28 15:44:35 +0800

    clean finish

--- verification.py
+++ verification.py
@@ -16,7 +16,7 @@ def easy_code_en(code_response):
     newFile = open("code_img.png", "wb")
     newFile.write(img_data)
     newFile.close()
-
+    # OCR reader
     reader = easyocr.Reader(['en']) # this needs to run only once to load the model into memory
     result = reader.readtext("code_img.png")
     code_res = result[0][-2].replace(" ", "")
